d4e870f7e79e4b63364fb10d4eb759c159ed5fb7
Far1d <fariid.yassine@gmail.com>
2024-08-23 03:11:37 +0200

    seedData and migrations

--- GameStore.API/Data/GameStoreContext.cs
+++ GameStore.API/Data/GameStoreContext.cs
@@ -6,6 +6,16 @@ namespace GameStore.API.Data;
 
 public class GameStoreContext(DbContextOptions<GameStoreContext> options) : DbContext(options)
 {
-    DbSet<Game> Games => Set<Game>();
-    DbSet<Genre> Genres => Set<Genre>();
+    public DbSet<Game> Games => Set<Game>();
+    public DbSet<Genre> Genres => Set<Genre>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Genre>().HasData(
+            new { Id = 1, Name = "Fighting" },
+            new { Id = 2, Name = "Sports" },
+            new { Id = 3, Name = "Racing" },
+            new { Id = 4, Name = "Kids and Family" }
+        );
+    }
 }
